4326a3ed4705cfe564ad022491b8b3e38f56335f
Hunter <huntercaouette@gmail.com>
2019-08-26 18:29:16 -0400

    more changes

--- headerScript.js
+++ headerScript.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  $('.header').load("header.html");
+  $('#header').load("header.html");
 });
+// To be sure $ is defined
+// Window loaded event
+// window.addEventListener("load", function(event) {
+//
+//   // Now $ or JQuery is completly available
+//   // Now using JQuery.load() should be defined
+//   $('#test').load('doesntmatter');
+//
+//   // Do what you want, the window is entirely loaded and ready to use.
+// });
